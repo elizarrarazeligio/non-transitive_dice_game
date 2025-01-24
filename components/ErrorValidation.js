255948b8e8efc26f2442ca265812cc6679d4dd3d
Eligio Elizarraraz <eligio2008@live.com.mx>
2025-01-24 14:18:36 -0600

    feat: finished validation before starting the game, to evaluate the user's dices input

--- components/ErrorValidation.js
+++ components/ErrorValidation.js
@@ -2,6 +2,10 @@ class ErrorValidation {
   initialInput(arr) {
     try {
       if (arr.length < 2) throw "Not enough dices to play.";
+      if (!arr.flat().every((i) => !isNaN(parseInt(i))))
+        throw "Do not enter non-integer values.";
+      if (!arr.reduce((prev, curr) => prev.length == curr.length))
+        throw "Not all dices have the same number of faces.";
     } catch (error) {
       console.log(error);
       process.exit();
