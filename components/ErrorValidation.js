d69af049472a764c0feb521e36edc09d31e073de
Eligio Elizarraraz <eligio2008@live.com.mx>
2025-01-24 14:30:14 -0600

    fix: corrected error handling when dices input have unequal faces number

--- components/ErrorValidation.js
+++ components/ErrorValidation.js
@@ -1,10 +1,17 @@
 class ErrorValidation {
   initialInput(arr) {
     try {
-      if (arr.length < 2) throw "Not enough dices to play.";
+      if (arr.length < 3) throw "Not enough dices to play.";
       if (!arr.flat().every((i) => !isNaN(parseInt(i))))
         throw "Do not enter non-integer values.";
-      if (!arr.reduce((prev, curr) => prev.length == curr.length))
+      if (
+        !arr.reduce((prev, curr) => {
+          if (prev.length == curr.length) {
+            return curr;
+          }
+          return false;
+        })
+      )
         throw "Not all dices have the same number of faces.";
     } catch (error) {
       console.log(error);
